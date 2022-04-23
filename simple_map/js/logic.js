da77147415b69022ff99d0d9b01d46e3a1cfda23
Ryan Williams <ryanwilliams@Ryans-MBP.attlocal.net>
2022-04-23 11:33:58 -0700

    simple map files

---
+++ simple_map/js/logic.js
@@ -0,0 +1,14 @@
+let myMap = L.map("mapid").setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+streets.addTo(myMap);
+
+var marker1 = L.marker([51.5, -0.09]).addTo(myMap)
