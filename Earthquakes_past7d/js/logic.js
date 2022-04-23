d240727001fac774364052e9bcf22799f7e712e5
Ryan Williams <ryanwilliams@Ryans-MBP.attlocal.net>
2022-04-23 16:35:12 -0700

    udated files to include earthquake json data

--- Earthquakes_past7d/js/logic.js
+++ Earthquakes_past7d/js/logic.js
@@ -1,8 +1,3 @@
-let baseMaps = {
-    "Streets": streets,
-    "Satelite": sateliteStreets
-}
-
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -12,18 +7,27 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/satelite-streets-v11',
     accessToken: API_KEY
 });
 
-// let map = L.map("myMap", {
-//     center: [39.5, -98.5],
-//     zoom: 3,
-//     layers: [streets]
-// });
+let baseMaps = {
+    "Streets": streets,
+    "Satelite": sateliteStreets
+};
+
+let map = L.map("mapid", {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    L.geoJSON(data).addTo(map);
+});
 
 // d3.json(torontoData).then(function(data){
 //     console.log(data);
