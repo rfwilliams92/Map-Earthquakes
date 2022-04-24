d81e382f79fb697dcd45cef43dad162fcd990867
Ryan Williams <ryanwilliams@Ryans-MBP.attlocal.net>
2022-04-23 17:41:22 -0700

    updated logic.js with adding earthquake overlay

--- Earthquakes_past7d/js/logic.js
+++ Earthquakes_past7d/js/logic.js
@@ -7,34 +7,74 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-let sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satelite-streets-v11',
+    id: 'mapbox/satellite-streets-v11',
     accessToken: API_KEY
 });
 
 let baseMaps = {
     "Streets": streets,
-    "Satelite": sateliteStreets
+    "Satelite": satelliteStreets
 };
-
+let earthquakes = new L.layerGroup();
+let overlays = {
+    Earthquakes: earthquakes
+};
 let map = L.map("mapid", {
     center: [39.5, -98.5],
     zoom: 3,
     layers: [streets]
 });
+L.control.layers(baseMaps, overlays).addTo(map);
 
+
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude * 4;
+}
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
+
+function styleInfo(feature) {
+    return {
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    }
+};
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-    L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
 });
-
-// d3.json(torontoData).then(function(data){
-//     console.log(data);
-//     L.geoJSON(data,{
-//         style: myStyle,
-//         onEachFeature: function (feature, layer) {
-//             layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-//         }
-//     }).addTo(myMap);
-// });
